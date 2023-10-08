3db9d771ff091ba2c3fd769b95a46ca23d72ba69
Monaphat374 <mybumbim2004@gmail.com>
2023-10-08 22:05:39 +0700

    finish unit test

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <InputPage></InputPage>
+    <FunctionPage></FunctionPage>
+  </div>
+</template>
+
+
+<script>
+import InputPage from 'src/pages/InputPage.vue'
+import FunctionPage from './FunctionPage.vue';
+export default {
+  name: 'HtmlComponent',
+  components:{
+    InputPage,
+    FunctionPage
+  },
+  data() {
+    return {
+      title: 'มนภัทร มั่นคง',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
